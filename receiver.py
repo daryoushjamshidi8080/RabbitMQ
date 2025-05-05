16491e77a8181ec04df05980b1bbd180935dcbb2
daryoushjamshidi8080 <daryoushjamshidi8080@gmail.com>
2025-05-05 20:01:56 +0330

    connect two file with rabbitmq;

--- receiver.py
+++ receiver.py
@@ -1,1 +1,16 @@
-print('hello world')
+import pika
+
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+ch = connection.channel()
+ch.queue_declare(queue='one')
+
+
+def callback(ch, method, properties, body):
+    print(f'Receinved  : {body}')
+
+
+ch.basic_consume(queue='one', on_message_callback=callback, auto_ack=True)
+print('Waiting for message, to exit press ctrl+c')
+ch.start_consuming()
--- sender.py
+++ sender.py
@@ -1,1 +1,13 @@
+import pika
 
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+ch = connection.channel()
+
+
+ch.queue_declare(queue='one')
+
+
+ch.basic_publish(exchange='', routing_key='one', body='Hello World ...')
+print('message send..!')
+connection.close()
