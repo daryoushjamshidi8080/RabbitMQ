266a12f7fea1efc0a9a62237d891b665834c9e0b
daryoushjamshidi8080 <daryoushjamshidi8080@gmail.com>
2025-05-09 16:58:06 +0330

    useing exchange fanout of rabbitmq broker

--- Fanout Exchange/receiver.py
+++ Fanout Exchange/receiver.py
@@ -1,1 +1,32 @@
+import pika
 
+
+credentials = pika.PlainCredentials('root', 'root')
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(
+        host='localhost',
+        port=5672,
+        virtual_host='/myapp',
+        credentials=credentials
+    )
+)
+ch = connection.channel()
+
+ch.exchange_declare(exchange='logs', exchange_type='fanout')
+result_queue = ch.queue_declare(queue='', exclusive=True)
+ch.queue_bind(exchange='logs', queue=result_queue.method.queue)
+
+print('Waiting for logs.. ')
+print(f'queue name : {result_queue.method.queue}')
+
+
+def callback(ch, method, properties, body):
+    print(f'message : {body}')
+
+
+ch.basic_consume(queue=result_queue.method.queue,
+                 on_message_callback=callback, auto_ack=True)
+
+
+ch.start_consuming()
--- Fanout Exchange/sender.py
+++ Fanout Exchange/sender.py
@@ -1,1 +1,21 @@
+import pika
 
+
+credentials = pika.PlainCredentials('root', 'root')
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(
+        host='localhost',
+        port=5672,
+        virtual_host='/myapp',
+        credentials=credentials
+    )
+)
+ch = connection.channel()
+ch.exchange_declare(exchange='logs', exchange_type='fanout')
+
+ch.basic_publish(exchange='logs', routing_key='', body='FANOUT..!')
+
+print('Message Send.')
+
+connection.close()
